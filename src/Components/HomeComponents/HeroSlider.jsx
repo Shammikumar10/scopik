cdc2e6b612c6190155574b689dd329dc7219d987
Shammikumar10 <shammi@thirdvizionlabs.com>
2026-02-23 18:16:10 +0530

    updated

--- src/Components/HomeComponents/HeroSlider.jsx
+++ src/Components/HomeComponents/HeroSlider.jsx
@@ -1,22 +1,13 @@
 import React, { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
 import slider1 from "../../assets/bnr.jpeg";
 import slider2 from "../../assets/bnr.jpeg";
 import slider3 from "../../assets/bnr.jpeg";
+import sliderMob from "../../assets/Mobnr.jpeg";
 
 const slides = [
-  {
-    id: 1,
-    image: slider1,
-  },
-  {
-    id: 2,
-    image: slider2,
-  },
-  {
-    id: 3,
-    image: slider3,
-  },
+  { id: 1, image: slider1 },
+  { id: 2, image: slider2 },
+  { id: 3, image: slider3 },
 ];
 
 const HeroSlider = () => {
@@ -30,38 +21,36 @@ const HeroSlider = () => {
   }, []);
 
   return (
-  <div className="relative w-100% h-[75vh] max-w-full ">
+    <div className="relative w-full h-[420px] md:h-[420px] mt-5 overflow-hidden">
       {slides.map((slide, index) => (
         <div
           key={slide.id}
-          className={`absolute inset-0 transition-opacity duration-1000 ease-in-out ${index === current ? "opacity-100 z-10" : "opacity-0 z-0"
-            }`}
+          className={`absolute inset-0 transition-opacity duration-1000 ${
+            index === current ? "opacity-100 z-10" : "opacity-0 z-0"
+          }`}
         >
-          {/* Background Image */}
           <img
             src={slide.image}
             alt=""
-            className="w-full h-full object-cover mt-10"
+            className="hidden md:block w-full h-[400px] object-cover mt-2"
           />
-
-          {/* Dark or Light Gradient Overlay */}
-          <div className="absolute inset-0 " />
-
-
-
+          <img
+            src={sliderMob}
+            alt=""
+            className="block md:hidden w-full h-96 object-cover mt-3"
+          />
         </div>
       ))}
-
-      {/* Slide Indicators */}
-      <div className="absolute bottom-6 left-1/2 transform -translate-x-1/2 flex gap-3 z-30">
+      <div className="absolute bottom-6 left-1/2 -translate-x-1/2 flex gap-3 z-30">
         {slides.map((_, index) => (
           <button
             key={index}
             onClick={() => setCurrent(index)}
-            className={`w-4 h-4 rounded-full border transition-all duration-300 ${index === current
-                ? "bg-orange-500 border-orange-500 scale-110 shadow-lg"
-                : "bg-gray-300/30 dark:bg-gray-700 border-gray-500 dark:border-gray-400"
-              }`}
+            className={`w-3 h-3 rounded-full transition ${
+              index === current
+                ? "bg-orange-500 scale-110"
+                : "bg-white/50"
+            }`}
           />
         ))}
       </div>
@@ -69,4 +58,4 @@ const HeroSlider = () => {
   );
 };
 
-export default HeroSlider
+export default HeroSlider;
