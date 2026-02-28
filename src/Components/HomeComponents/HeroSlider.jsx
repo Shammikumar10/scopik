042eb9b64d01ee9f91a230bd44f27bdccb670633
Shammikumar10 <shammi@thirdvizionlabs.com>
2026-02-28 12:58:08 +0530

    updated

--- src/Components/HomeComponents/HeroSlider.jsx
+++ src/Components/HomeComponents/HeroSlider.jsx
@@ -1,42 +1,47 @@
 import React, { useState, useEffect } from "react";
-import slider1 from "../../assets/bnr.jpeg";
-import slider2 from "../../assets/bnr.jpeg";
-import slider3 from "../../assets/bnr.jpeg";
-import sliderMob from "../../assets/Mobnr.jpeg";
+import { useNavigate } from "react-router-dom";
+
+import slider1 from "../../assets/HeroImages/Web.jpeg";
+import slider2 from "../../assets/HeroImages/Web.jpeg";
+import slider3 from "../../assets/HeroImages/Web.jpeg";
+import sliderMob from "../../assets/HeroImages/mobile2.jpeg";
 
 const slides = [
-  { id: 1, image: slider1 },
-  { id: 2, image: slider2 },
-  { id: 3, image: slider3 },
+  { id: 1, image: slider1, link: "/course/33" },
+  { id: 2, image: slider2, link: "/course/34" },
+  { id: 3, image: slider3, link: "/course/35" },
 ];
 
 const HeroSlider = () => {
   const [current, setCurrent] = useState(0);
+  const navigate = useNavigate();
 
   useEffect(() => {
     const interval = setInterval(() => {
       setCurrent((prev) => (prev + 1) % slides.length);
     }, 5000);
+
     return () => clearInterval(interval);
   }, []);
 
   return (
-    <div className="relative w-full h-[420px] md:h-[420px] mt-5 overflow-hidden">
+    <div className="relative w-full h-[420px] mt-5 overflow-hidden">
       {slides.map((slide, index) => (
         <div
           key={slide.id}
-          className={`absolute inset-0 transition-opacity duration-1000 ${
+          onClick={() => navigate(slide.link)}
+          className={`absolute inset-0 cursor-pointer transition-opacity duration-1000 ${
             index === current ? "opacity-100 z-10" : "opacity-0 z-0"
           }`}
         >
           <img
             src={slide.image}
-            alt=""
+            alt="slider"
             className="hidden md:block w-full h-[400px] object-cover mt-2"
           />
           <img
             src={sliderMob}
-            alt=""
+            alt="slider mobile"
             className="block md:hidden w-full h-96 object-cover mt-3"
           />
         </div>
